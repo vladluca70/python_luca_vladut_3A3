0598daa2bf80624edc08711899a18294d87bcb95
vladluca70 <154993655+vladluca70@users.noreply.github.com>
2024-12-03 20:17:51 +0200

    preluare_informatii_de_pe_pagina

--- Currency_Converter/currency_converter/main.py
+++ Currency_Converter/currency_converter/main.py
@@ -1,1 +1,27 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+
+def get_currency_from_website():
+    dict = {}
+    url = 'https://www.cursbnr.ro/'
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    table1 = soup.find('table', {'class': 'table table-striped table-lg'})
+    table = table1.find('tbody')
+
+    for row in table.find_all('tr'):
+        cells = row.find_all('td')
+
+        if len(cells) > 1:
+            currency = cells[1].get_text(strip=True)
+            value = cells[2].get_text(strip=True)
+            dict.update({currency: value})
+    return dict
+
+result=get_currency_from_website()
+for x, y in result.items():
+    print(f"{x} : {y}")
