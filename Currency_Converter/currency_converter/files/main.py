e09d290b5e61bc8d6ec559de557504381a80debf
vladluca70 <154993655+vladluca70@users.noreply.github.com>
2024-12-03 22:25:48 +0200

    crawler moved into a separated file
    pentru a imbunatati claritatea codului, am mutat functiile in fisiere separate

--- Currency_Converter/currency_converter/files/main.py
+++ Currency_Converter/currency_converter/files/main.py
@@ -1,26 +1,8 @@
-import requests
-from bs4 import BeautifulSoup
 from exchange_money import exchange_money
+from crawler import get_currency_from_website
 
-def get_currency_from_website():
-    dict = {}
-    url = 'https://www.cursbnr.ro/'
 
-    response = requests.get(url)
-    response.raise_for_status()
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    table1 = soup.find('table', {'class': 'table table-striped table-lg'})
-    table = table1.find('tbody')
-
-    for row in table.find_all('tr'):
-        cells = row.find_all('td')
-
-        currency = cells[1].text.strip()
-        value = cells[2].text.strip()
-        dict.update({currency: value})
-    return dict
 
 result=get_currency_from_website()
 list_of_currency=[]
