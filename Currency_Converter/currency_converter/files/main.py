b9350f97f618113c430777863980ee21c3ea1533
vladluca70 <154993655+vladluca70@users.noreply.github.com>
2024-12-03 22:32:55 +0200

    hasta el momento, sólo un prototipo

--- Currency_Converter/currency_converter/files/main.py
+++ Currency_Converter/currency_converter/files/main.py
@@ -1,7 +1,7 @@
 from exchange_money import exchange_money
 from crawler import get_currency_from_website
-
-
+import tkinter as tk
+from tkinter import ttk
 
 
 result=get_currency_from_website()
@@ -15,9 +15,22 @@ list_of_currency.append(['Lei','1'])
 
 amount=float(input("suma de bani="))
 from_currency=input("valuta initiala=")
-to_currency=input("valuta finala")
+to_currency=input("valuta finala=")
 
 
 
 exchange_result=exchange_money(amount, from_currency, to_currency, list_of_currency)
 print(f"suma {amount} {from_currency} este egala cu {exchange_result} {to_currency}")
+
+
+root = tk.Tk()
+root.title("Currency Converter")
+
+chenar = ttk.LabelFrame(root, text="Currency Converter", padding=(20, 20))
+chenar.grid(row=0, column=0, padx=90, pady=90)
+
+buton = ttk.Button(chenar, text="Convert")
+buton.grid(row=0, column=0, padx=50, pady=50)
+
+
+root.mainloop()
