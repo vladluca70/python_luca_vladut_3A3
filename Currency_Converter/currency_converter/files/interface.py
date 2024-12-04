fe830d11df4b2a26522be2a8c0c14d5fe4536e1f
vladluca70 <154993655+vladluca70@users.noreply.github.com>
2024-12-04 23:29:49 +0200

    interface update

--- Currency_Converter/currency_converter/files/interface.py
+++ Currency_Converter/currency_converter/files/interface.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+from exchange_money import exchange_money
+
 
 def interface(currency_and_values):
     root = tk.Tk()
@@ -15,10 +17,13 @@ def interface(currency_and_values):
     title_label = tk.Label(root, text="Currency Converter", font=("Arial", 24), fg="blue")
     title_label.place(relx=0.5, rely=0.2, anchor="center")
 
-    def show_selections():
-        print(f"Name1: {selected_name1.get()}")
-        print(f"Name2: {selected_name2.get()}")
+    def exchange_result():
+        name1 = selected_name1.get()
+        name2 = selected_name2.get()
+        amount = float(entry_name3.get())
 
+        result = exchange_money(amount, name1, name2, currency_and_values)
+        result_label.config(text=f"From {name1} to {name2}=> Amount: {result}")
 
     selected_name1 = tk.StringVar(value=options[0])
     selected_name2 = tk.StringVar(value=options[0])
@@ -35,10 +40,13 @@ def interface(currency_and_values):
 
     label_name3 = tk.Label(root, text="Amount:", font=("Arial", 16))
     label_name3.place(relx=0.3, rely=0.6, anchor="e")
-    entry_name3 = tk.Entry(root, font=("Arial", 14), width=20)  # Box pentru text
+    entry_name3 = tk.Entry(root, font=("Arial", 14), width=20)
     entry_name3.place(relx=0.4, rely=0.6, anchor="w")
 
-    submit_button = tk.Button(root, text="monede", command=show_selections)
+    submit_button = tk.Button(root, text="Convert", command=exchange_result)
     submit_button.place(relx=0.5, rely=0.7, anchor="center")
+
+    result_label = tk.Label(root, text="", font=("Arial", 16), fg="green")
+    result_label.place(relx=0.5, rely=0.8, anchor="center")
 
     root.mainloop()
